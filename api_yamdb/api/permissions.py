b1ba79a65c89a6bee5d52b214be50bf8b24f74dd
Антон Гридасов <aogridasov@gmail.com>
2022-08-13 18:10:33 +0300

    latest version

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,13 +1,9 @@
 from rest_framework import permissions
 
 
-<<<<<<< HEAD
 class IsAuthorOrReadOnly(permissions.BasePermission):
 
     message = 'Изменение чужого контента запрещено!'
-=======
-class IsAllowedToEditOrReadOnly(permissions.BasePermission):
->>>>>>> 0340f64 (commit before pulling from updated master)
 
     def has_object_permission(self, request, view, obj):
         return (
