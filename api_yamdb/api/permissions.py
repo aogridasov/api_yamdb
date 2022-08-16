d98d14f1bed90ca6218a0d7855dea7af221c4ea4
Антон Гридасов <aogridasov@gmail.com>
2022-08-16 19:41:29 +0300

    fixed permission HasRightsOrReadOnly logic

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -22,15 +22,21 @@ class IsAdmin(BasePermission):
 
 
 class HasRightsOrReadOnly(BasePermission):
+    message = 'Вы не авторизованы на данный запрос, sorry buddy'
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         return (
             request.method in SAFE_METHODS
-            or (
-                request.user.is_authenticated and (
-                    obj.author == request.user
-                    or request.user.is_moderator
-                    or request.user.is_admin
-                )
-            )
+            or request.user.is_authenticated
         )
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or (request.user.is_authenticated
+                    and (
+                        obj.author == request.user
+                        or request.user.is_moderator
+                        or request.user.is_admin
+                    )
+                    )
+                )
